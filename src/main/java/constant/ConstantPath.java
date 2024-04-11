ab144b8eb32c01c8e3ebb354887c32d5408c099f
Vivek kumar <vivekkumarqa2991@gmail.com>
2024-04-11 16:29:39 +0530

    test1

--- src/main/java/constant/ConstantPath.java
+++ src/main/java/constant/ConstantPath.java
@@ -12,4 +12,5 @@ public class ConstantPath {
 	public static final String CHROMEDRIVER = "repace with chromedriver path";
 	public static final String EDGEDRIVER = "repace with edgedriver path";
 	public static final String GICODRIVER = "repace with gicodriver path";
+	// seqrite test
 }
